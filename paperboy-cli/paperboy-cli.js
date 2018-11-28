34e69642310301b91cdbc932ed5a1ae1e302c14f
Arne Diekmann <arne@neoskop.de>
2018-11-28 10:45:21 +0100

    feat(cli): enable non-amqp execution.

--- paperboy-cli/paperboy-cli.js
+++ paperboy-cli/paperboy-cli.js
@@ -1,37 +1,58 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const fs = require('fs');
-const Paperboy = require('@neoskop/paperboy').Paperboy;
-const program = require('commander');
-const PaperboyMagnoliaSource = require('@neoskop/paperboy-source-magnolia');
+const chalk = require("chalk");
+const fs = require("fs");
+const Paperboy = require("@neoskop/paperboy").Paperboy;
+const program = require("commander");
+const PaperboyMagnoliaSource = require("@neoskop/paperboy-source-magnolia");
 
-program.version('0.11.0').description('Paperboy CLI');
+program.version("0.11.0").description("Paperboy CLI");
 
 program
-  .command('start')
-  .description('')
-  .usage('[options]')
-  .option('-c --config [config]', 'Path to config file')
+  .command("build")
+  .description("Fetches data and builds the frontend only once")
+  .option("-c --config [config]", "Path to config file")
   .action(program => {
-    let config, paperboy;
-    let configPath = program.config || 'paperboy.config.json';
-
-    console.log(chalk.bold.cyan('[Paperboy] Starting …'));
-
-    try {
-      config = JSON.parse(fs.readFileSync(configPath));
-    } catch (e) {
-      throw e;
-    }
-
-    paperboy = new Paperboy({
-      readinessHook: config.readinessHook,
-      source: Object.assign(config.sourceOptions, { sourceFactory: PaperboyMagnoliaSource }),
-      sink: config.sinkOptions
+    const paperboy = setupPaperboy(program, config => {
+      // Forcefully disable async process spawning
+      config.sinkOptions.async = false;
     });
+    paperboy.build();
+  });
 
+program
+  .command("start")
+  .description(
+    "Continuously watches an AMQP queue and rebuilds the frontend when a message is received"
+  )
+  .option("-c --config [config]", "Path to config file")
+  .action(program => {
+    const paperboy = setupPaperboy(program);
     paperboy.start();
   });
 
 program.parse(process.argv);
+
+function setupPaperboy(program, configModifier) {
+  let config;
+  let configPath = program.config || "paperboy.config.json";
+  console.log(chalk.bold.cyan("[Paperboy] Starting …"));
+  try {
+    config = JSON.parse(fs.readFileSync(configPath));
+  } catch (e) {
+    throw e;
+  }
+
+  if (configModifier) {
+    configModifier(config);
+  }
+
+  const paperboy = new Paperboy({
+    readinessHook: config.readinessHook,
+    source: Object.assign(config.sourceOptions, {
+      sourceFactory: PaperboyMagnoliaSource
+    }),
+    sink: config.sinkOptions
+  });
+  return paperboy;
+}
--- paperboy-core/src/paperboy.ts
+++ paperboy-core/src/paperboy.ts
@@ -1,10 +1,10 @@
-import { ExecOutputReturnValue } from 'shelljs';
-import * as winston from 'winston';
-import * as shelljs from 'shelljs';
+import { ExecOutputReturnValue } from "shelljs";
+import * as winston from "winston";
+import * as shelljs from "shelljs";
 
-import { PaperboyOptions } from './interfaces/paperboy-options.interface';
-import { Source } from './interfaces/source.interface';
-import { Sink } from './sink';
+import { PaperboyOptions } from "./interfaces/paperboy-options.interface";
+import { Source } from "./interfaces/source.interface";
+import { Sink } from "./sink";
 
 export class Paperboy {
   private options: PaperboyOptions;
@@ -17,14 +17,18 @@ export class Paperboy {
   }
 
   public async fromSource(): Promise<void> {
+    if (!this.source) {
+      this.source = await this.loadSource();
+    }
+
     return this.source.generate().catch((err: any) => {
-      winston.error('Generation from source failed', err);
+      winston.error("Generation from source failed", err);
     });
   }
 
   public async toSink(): Promise<void | ExecOutputReturnValue> {
     return this.sink.build().catch((err: any) => {
-      winston.error('Building to sink failed', err);
+      winston.error("Building to sink failed", err);
     });
   }
 
@@ -41,7 +45,7 @@ export class Paperboy {
 
     // Execute readiness hook if set
     if (this.options.readinessHook && 0 !== this.options.readinessHook.length) {
-        shelljs.exec(this.options.readinessHook);
+      shelljs.exec(this.options.readinessHook);
     }
 
     // notify source of completed initial build
@@ -49,6 +53,9 @@ export class Paperboy {
   }
 
   private async loadSource(): Promise<Source> {
-    return this.options.source.sourceFactory.buildSource(this.options.source, this.toSink.bind(this));
+    return this.options.source.sourceFactory.buildSource(
+      this.options.source,
+      this.toSink.bind(this)
+    );
   }
 }
