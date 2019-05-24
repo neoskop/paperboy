2b40aa247e0455d2647c3d69d6c6a0beab9e7a35
Arne Diekmann <arne@neoskop.de>
2019-05-24 12:26:48 +0200

    feat: push docker image of push service and version magnolia module

--- release.sh
+++ release.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+function check_command() {
+  if ! command -v $1 >/dev/null; then
+    echo -e "Install \033[1m$1\033[0m"
+    exit 1
+  fi
+}
+
+check_command mvn
+check_command jq
+check_command npm
+check_command docker
+
 if [[ "$#" != "1" ]] || [[ ! "$1" =~ ^(patch|minor|major)$ ]]; then
   echo "Usage: $0 patch|minor|major"
   exit 1
@@ -20,6 +32,10 @@ npm version $1
 version=`cat package.json | jq -r .version`
 npm publish
 
+cd ../paperboy-magnolia-module
+mvn versions:set -DnewVersion=${version} -DgenerateBackupPoms=false >/dev/null
+find . -name "pom.xml" -exec git add '{}' \;
+
 cd ../paperboy-cli
 cat package.json | jq ".version = \"$version\" | .dependencies.\"@neoskop/paperboy\" = \"$version\"" > package.json.new
 mv package.json.new package.json
@@ -35,6 +51,9 @@ mv package.json.new package.json
 npm i
 npm run build
 npm publish
+docker build -t neoskop/paperboy-push-service:$version .
+docker push neoskop/paperboy-push-service:$version
+docker push neoskop/paperboy-push-service:latest
 cd ../
 
 git add .
