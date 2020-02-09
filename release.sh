52750cb2137684ba1755323620d258d76f7f64d9
Arne Diekmann <arne@neoskop.de>
2020-02-09 16:11:29 +0100

    feat: release helm chart in release script

--- release.sh
+++ release.sh
@@ -10,6 +10,8 @@ function check_command() {
 
 check_command mvn
 check_command jq
+check_command yq
+check_command yarn
 check_command npm
 check_command docker
 
@@ -18,7 +20,7 @@ if [[ "$#" != "1" ]] || [[ ! "$1" =~ ^(patch|minor|major)$ ]]; then
   exit 1
 fi
 
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
   echo -e "The repository has changes. Commit first...\033[0;31mAborting!\033[0m"
   exit 1
 fi
@@ -34,15 +36,18 @@ cd ../paperboy-core
 yarn
 yarn build
 npm version $1
-version=`cat package.json | jq -r .version`
+version=$(cat package.json | jq -r .version)
 npm publish
 
 cd ../paperboy-magnolia-module
 mvn versions:set -DnewVersion=${version} -DgenerateBackupPoms=false
 mvn deploy
 
+cd ../paperboy-docker
+sed -i "s/ENV PAPERBOY_VERSION=[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/ENV PAPERBOY_VERSION=$version/" paperboy-docker/Dockerfile
+
 cd ../paperboy-cli
-cat package.json | jq ".version = \"$version\" | .dependencies.\"@neoskop/paperboy\" = \"$version\"" > package.json.new
+cat package.json | jq ".version = \"$version\" | .dependencies.\"@neoskop/paperboy\" = \"$version\"" >package.json.new
 mv package.json.new package.json
 yarn
 
@@ -51,7 +56,7 @@ rm -rf paperboy-cli.js.bak
 npm publish
 
 cd ../paperboy-push-service
-cat package.json | jq ".version = \"$version\"" > package.json.new
+cat package.json | jq ".version = \"$version\"" >package.json.new
 mv package.json.new package.json
 yarn
 yarn build
@@ -60,11 +65,26 @@ docker build -t neoskop/paperboy-push-service:$version .
 docker build -t neoskop/paperboy-push-service:latest .
 docker push neoskop/paperboy-push-service:$version
 docker push neoskop/paperboy-push-service:latest
-cd ../
 
+cd ../paperboy-helm
+yq w -i ./Chart.yaml version $version
+yq w -i ./Chart.yaml appVersion $version
+yq w -i ./values.yaml image.tag $version
+
+cd ../
 git add .
-git commit -m "chore(): Bump version to ${version}."
+git commit -m "chore: Bump version to ${version}."
 git tag ${version}
 git push origin $version
 git pull --rebase
 git push
+
+helm package helm --destination .deploy
+cr upload -o neoskop -r paperboy -p .deploy
+git checkout gh-pages
+cr index -i ./index.yaml -p .deploy -o neoskop -r paperboy -c https://neoskop.github.io/paperboy/
+git add index.yaml
+git commit -m "chore: Bump version to ${version}."
+git push
+git checkout master
+rm -rf .deploy/
